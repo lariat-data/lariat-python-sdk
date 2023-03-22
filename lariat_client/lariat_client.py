33b40f98abe5ffdb0c38cc99776479afb029150f
Nishant Kant Gupta <nishant.kant.gupta@gmail.com>
2023-03-22 04:36:15 -0400

    documentation added

--- lariat_client/lariat_client.py
+++ lariat_client/lariat_client.py
@@ -105,7 +105,7 @@ class RawDataset:
 
     Attributes:
         source_id (str): The unique ID of the data source.
-        data_source (str): The type of data source (e.g., 'csv', 'database').
+        data_source (str): The type of data source (e.g., 'athena').
         name (str): The name of the raw dataset.
         schema (Dict): The schema of the raw dataset.
     """
@@ -119,6 +119,14 @@ class RawDataset:
         return json.dumps(self.__dict__)
 
 class Dataset:
+    """A class representing a dataset in Lariat.
+
+    Attributes:
+        data_source (str): The type of data source (e.g., 'athena').
+        source_id (str): The unique ID of the data source.
+        name (str): The name of the raw dataset.
+        schema (Dict): The schema of the raw dataset.
+    """
     def __init__(self, data_source: str, source_id: str, name: str, id: int, query: str, schema: json):
         self.data_source = data_source
         self.source_id = source_id
@@ -130,17 +138,41 @@ class Dataset:
     def __repr__(self):
         return json.dumps(self.__dict__)
 
-    def get_schema(self):
+    def get_schema(self) -> json:
+        """
+        Gets the schema for the dataset.
+
+        Returns:
+            json: A json representation of the dataset schema.
+        """
         return self.schema
 
-    def get_schema_fields(self):
+    def get_schema_fields(self) -> List[Field]:
+        """
+        Gets the schema's fields for the dataset.
+
+        Returns:
+            List[Field]: A list of the flattened schema fields of the dataset.
+        """
         flattened_keys = [flatten(d, '.') for d in [self.schema]][0].keys()
         return [Field(dataset_id=self.id, name=field) for field in flattened_keys]
 
     def get_indicators(self) -> List[Indicator]:
+        """
+        Gets the indicators that query this dataset.
+
+        Returns:
+            List[Indicator]: A list of the indicators that query this dataset.
+        """
         return get_indicators(datasets=[self])
 
 class Field:
+    """A class representing a dataset's field in Lariat.
+
+    Attributes:
+        dataset_id (int): the unique ID of a dataset.
+        name (str): The name of the field.
+    """
     def __init__(self, dataset_id: int, name: str):
         self.dataset_id = dataset_id
         self.name = name
@@ -149,6 +181,13 @@ class Field:
         return json.dumps(self.__dict__)
 
 class FilterClause:
+    """A class representing a clause to filter metrics by.
+
+    Attributes:
+        field (str): The field to filter on.
+        operator (str): The operator to apply to the filter values. (in, eq, not_in, neq, etc.)
+        values (list or str): A string or list of values to filter on.
+    """
     def __init__(self, field: str, operator: str, values: Union[str, List[str]]):
         self.field = field
         self.operator = operator
@@ -159,6 +198,12 @@ class FilterClause:
 
 
 class Filter:
+    """A class representing a filter to use when querying metrics.
+
+    Attributes:
+        clauses (list): A list of filter clauses to apply when querying metrics.
+        operator (str): The operator to apply to the filter clauses. (AND, OR, etc.)
+    """
     def __init__(self, clauses: List[FilterClause], operator: str):
         self.clauses = clauses
         self.operator = operator
@@ -167,6 +212,13 @@ class Filter:
         return json.dumps(self.__dict__)
 
 class MetricRecord:
+    """A class representing a record returned when querying metrics.
+
+    Attributes:
+        evaluation_time (int): The epoch timestamp associated with the indicator's evaluation.
+        value (str): The value of the metric at this evaluation time.
+        dimensions (dict): A dictionary mapping of a metric's dimensional names to values.
+    """
     def __init__(self, evaluation_time: int, value: float, dimensions: Dict[str, str]):
         self.evaluation_time = evaluation_time
         self.value = value
@@ -176,6 +228,13 @@ class MetricRecord:
         return json.dumps(self.__dict__)
 
     def to_dict(self) -> Dict[str, str]:
+        """
+        Gets a dictionary representation of the metric record.
+
+        Returns:
+            Dict[str, str]: A dictionary holding the evalution time, value, and
+                dimensions of the metric record.
+        """
         record_dict = {
             'evaluation_time': self.evaluation_time,
             'value': self.value
@@ -185,6 +244,12 @@ class MetricRecord:
         return record_dict
 
 class MetricRecordList:
+    """A class representing a collection of metric records.
+
+    Attributes:
+        group_by_fields (list): The epoch timestamp associated with the indicator's evaluation.
+        records (list): A list of metric records returned by a query.
+    """
     def __init__(self, group_by_fields: List[str], records):
         self.group_by_fields = group_by_fields
         self.records = [MetricRecord(**record) for record in records]
@@ -203,10 +268,20 @@ class MetricRecordList:
         self.index += 1
         return record
 
-    def to_df(self):
+    def to_df(self) -> pd.DataFrame:
+        """
+        Converts the MetricRecord list into a pandas dataframe.
+
+        Returns:
+            Pandas DataFrame: A Dataframe consisting of the metric records
+                returned by an indicator query.
+        """
         return pd.DataFrame.from_records([record.to_dict() for record in self.records])
 
     def to_csv(self, filename, header=True):
+        """
+        Writes the MetricRecord list to a csv.
+        """
         with open(filename, mode='w', newline='') as file:
             writer = csv.writer(file)
             if header:
@@ -219,14 +294,17 @@ class MetricRecordList:
                 writer.writerow([vals[field] for field in output_array])
 
         
-
-def configure(api_key: str, application_key: str):
-    s.headers.update({
-        'X-Lariat-Application-Key': application_key,
-        'X-Lariat-Api-Key': api_key,
-    })
-
 def get_raw_datasets(dataset_ids: List[int]) -> List[RawDataset]:
+    """
+    Gets the raw datasets given a list of dataset ids.
+
+    Args:
+        dataset_ids (list): A list of dataset ids to filter on.
+
+    Returns:
+        List[RawDataset]: A list of raw datasets that the provided dataset_ids
+            are created from.
+    """
     try:
         r = s.get(f'{LARIAT_PUBLIC_API_ENDPOINT}/raw-datasets',
             params={'dataset_id': dataset_ids})
@@ -247,6 +325,16 @@ def get_raw_datasets(dataset_ids: List[int]) -> List[RawDataset]:
         sys.exit(1)
 
 def get_dataset(name: str, source_id: str) -> Union[Dataset, None]:
+    """
+    Gets the dataset corresponding to a given name source_id.
+
+    Args:
+        name: The name of the dataset.
+        source_id: The source of the dataset.
+
+    Returns:
+        Union[Dataset,None]: A dataset corresponding to the given name and source.
+    """
     try:
         r = s.get(f'{LARIAT_PUBLIC_API_ENDPOINT}/datasets',
             params={'source_id': source_id, 'name': name})
@@ -271,6 +359,15 @@ def get_dataset(name: str, source_id: str) -> Union[Dataset, None]:
         sys.exit(1)
 
 def get_datasets(name: str = None) -> List[Dataset]:
+    """
+    Gets the datasets corresponding to a given name source_id.
+
+    Args:
+        name (optional str): The name of the dataset.
+
+    Returns:
+        List[Dataset]: A list of datasets belonging to the user.
+    """
     try:
         r = s.get(f'{LARIAT_PUBLIC_API_ENDPOINT}/datasets', params={'name': name})
         r.raise_for_status()
@@ -291,7 +388,18 @@ def get_datasets(name: str = None) -> List[Dataset]:
         logging.error(f"Something went wrong: {err}")
         sys.exit(1)
 
-def get_indicators(datasets: List[Dataset] = [], tags: List[str] = [], fields: List[Field] = []):
+def get_indicators(datasets: List[Dataset] = [], tags: List[str] = [], fields: List[Field] = []) -> List[Indicator]:
+    """
+    Gets the indicators corresponding to optional filters.
+
+    Args:
+        datasets (list): A list of datasets to get corresponding indicators for.
+        tags (list): A list of tags to filter the indicators by.
+        fields (list): A list of fields that the indicators should include.
+
+    Returns:
+        List[Indicator]: A list of corresponding indicators.
+    """
     params = {}
     if datasets:
         params['dataset_id'] = [dataset.id for dataset in datasets]
@@ -331,6 +439,15 @@ def get_indicators(datasets: List[Dataset] = [], tags: List[str] = [], fields: L
         sys.exit(1)
 
 def get_indicator(id: int) -> Indicator:
+    """
+    Gets the indicator corresponding to the provided id.
+
+    Args:
+        id (int): The indicator unique identifier.
+
+    Returns:
+        Indicator: The indicator corresponding to the provided id.
+    """
     params = {'indicator_id': id}
     try:
         r = s.get(f'{LARIAT_PUBLIC_API_ENDPOINT}/indicator', params=params)
@@ -369,7 +486,22 @@ def query(
     aggregate: str = None,
     query_filter: Filter = None,
     output_format: str = 'json'
-    ):
+    ) -> MetricRecordList:
+    """
+    Queries a provided indicator for its metric data,.
+
+    Args:
+        indicator (indicator): Indicator to query.
+        from_ts (datetime.datetime): The start time for the indicator evaluation.
+        to_ts (datetime.datetime): The end time for the indicator evaluation.
+        group_by (list): A list of strings to group the metrics data by.
+        aggregate (str): An optional aggregation function to apply to the metric.
+        query_filter (filter): A filter function to apply to the metric.
+        output_format (str): The format to output the query results as.
+
+    Returns:
+        MetricRecordList: An object that contains the list of records output by the query.
+    """
     data_filter = {
         'operator': 'or',
         'filters': []
