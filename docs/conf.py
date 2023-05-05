f404ac78d3fc5816007ec741986488e3ea2d60d5
Vikas Shanbhogue <vikas.shanbhogue@gmail.com>
2023-05-04 23:49:41 -0400

    Added .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -6,34 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lariat Python SDK'
-copyright = '2023, Lariat Data'
-author = 'Lariat Data'
+project = "Lariat Python SDK"
+copyright = "2023, Lariat Data"
+author = "Lariat Data"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
-extensions = ['sphinx.ext.napoleon']
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc"]
 
 napoleon_google_docstring = True
 napoleon_use_param = True
 napoleon_use_ivar = True
-
-import sys 
-import os 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
